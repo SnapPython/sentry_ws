55c692282cf327269029f82f74c687807eac6626
SnapPython <112377633+SnapPython@users.noreply.github.com>
2023-11-14 16:57:29 +0800

    fix

---
+++ src/src/rm_auto_aim/auto_aim_interfaces/build/auto_aim_interfaces__py/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/mechax/ws_y/mechax_rm_cv/src/rm_auto_aim/auto_aim_interfaces/build/auto_aim_interfaces__py
+# Build directory: /home/mechax/ws_y/mechax_rm_cv/src/rm_auto_aim/auto_aim_interfaces/build/auto_aim_interfaces__py
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
